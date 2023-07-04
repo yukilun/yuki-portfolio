e951551c2f5c099df2e9423960052cf08378c810
yukilun <102889935+yukilun@users.noreply.github.com>
2023-07-04 02:05:26 -0700

    initial projects page

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,14 +1,27 @@
 <template>
-    <div class="content-container">
-        Projects
-        <p class="text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum placeat blanditiis molestias quasi quo! Ad numquam, vero qui deserunt id veritatis nemo, placeat iure amet sit molestiae? Possimus sint tempore nobis magnam incidunt, laborum nihil quae deserunt aperiam, alias, reiciendis officia repudiandae nostrum placeat saepe nemo aliquid fugit consequuntur hic maxime amet? Fugit culpa reprehenderit eligendi natus. Nihil voluptas dolores laudantium eveniet perferendis. Soluta consequatur totam aliquam facere iste optio nesciunt placeat quas ducimus atque voluptatibus illum veniam sequi esse, id quis sit nemo! Libero, possimus voluptate? Veniam unde esse rerum, earum autem sunt odit exercitationem rem similique, eos quasi!</p>
-        <p class="text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum placeat blanditiis molestias quasi quo! Ad numquam, vero qui deserunt id veritatis nemo, placeat iure amet sit molestiae? Possimus sint tempore nobis magnam incidunt, laborum nihil quae deserunt aperiam, alias, reiciendis officia repudiandae nostrum placeat saepe nemo aliquid fugit consequuntur hic maxime amet? Fugit culpa reprehenderit eligendi natus. Nihil voluptas dolores laudantium eveniet perferendis. Soluta consequatur totam aliquam facere iste optio nesciunt placeat quas ducimus atque voluptatibus illum veniam sequi esse, id quis sit nemo! Libero, possimus voluptate? Veniam unde esse rerum, earum autem sunt odit exercitationem rem similique, eos quasi!</p>
+    <div class="content-container" :class="isLoading && 'h-0 overflow-hidden'" ref="container">
+        <div class="projects w-[90%] mx-auto max-w-[1200px]">
+            <SectionHeader heading="Projects" :canScroll="canScroll" />
+            <div class="projects-table">
+                <ProjectsRow v-for="project in data.projects" :project="project" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
-    const loaderText = useLoaderText();
-    loaderText.value = 'Projects';
+import data from '~/assets/data.json';
+
+const isLoading = useIsLoading();
+const loaderText = useLoaderText();
+loaderText.value = 'Projects';
+
+const canScroll = ref(true);
+const container = ref(null);
+
+onMounted(()=> {
+    canScroll.value = container.value.scrollHeight > document.body.clientHeight;
+})
 </script>
 
 <style scoped>
