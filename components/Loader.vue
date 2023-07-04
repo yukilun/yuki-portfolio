af17b575243f31129d1c8c9c9dd5f8b005d111fb
yukilun <102889935+yukilun@users.noreply.github.com>
2023-07-04 02:03:52 -0700

    changing isLoading state from loader

--- components/Loader.vue
+++ components/Loader.vue
@@ -1,21 +1,33 @@
 <template>
-    <div class="loader fixed w-full h-screen z-40 bg-[#617079] flex flex-col justify-center items-center gap-6" :class="isActive && 'active'">
+    <div class="loader fixed w-full h-screen z-40 bg-[#617079] flex flex-col justify-center items-center gap-6"
+        :class="isActive && 'active'">
         <LoaderBubbles />
-        <LoaderText/>
+        <LoaderText />
     </div>
 </template>
 
 <script setup>
-    const isActive = ref(true);
-    const timer = ref();
-    onMounted(()=> {
-        timer.value = setTimeout(()=> {
-            isActive.value = false;
-        }, 2500);
-    })
-    onUnmounted(() => {
-        clearTimeout(timer);
-    })
+const isLoading = useIsLoading();
+const isActive = ref(true);
+const timerLoading = ref(); 
+const timerActive = ref();
+onMounted(() => {
+    timerActive.value = setTimeout(() => {
+        isActive.value = false;
+        timerLoading.value = setTimeout(()=> {
+            isLoading.value = false;
+        }, 500);
+    }, 2500);
+});
+onUnmounted(() => {
+    clearTimeout(timerLoading);
+    clearTimeout(timerActive);
+});
+
+const route = useRoute();
+watch(route, value => {
+    isLoading.value = true
+}, { deep: true, immediate: true })
 </script>
 
 <style scoped>
@@ -25,12 +37,12 @@
     bottom: 0;
     transform: translateY(-100%);
     opacity: 0;
-	transition: all 1500ms cubic-bezier(.8,-.2,.2,1);
+    transition: all 1500ms cubic-bezier(.8, -.2, .2, 1);
 }
 
 .loader.active {
     transform: translateY(0) !important;
-	opacity: 100%;
-    transition: all 1500ms cubic-bezier(.8,-.2,.2,1);
+    opacity: 100%;
+    transition: all 1500ms cubic-bezier(.8, -.2, .2, 1);
 }
 </style>
