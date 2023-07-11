642f0bae546fe37bd481dff29a7499072a5d2365
yukilun <102889935+yukilun@users.noreply.github.com>
2023-07-11 00:43:20 -0700

    add about content

--- pages/about.vue
+++ pages/about.vue
@@ -1,14 +1,32 @@
 <template>
-    <div class="content-container">
-        About
+    <div class="content-container" :class="isLoading && 'h-0 overflow-hidden'" ref="container">
+        <div class="about w-[90%] mx-auto">
+            <SectionHeader heading="About" :canScroll="canScroll" />
+            <div class="about-content max-w-[900px] mx-auto">
+                <AboutShortBio />
+                <AboutMyJourney />
+                <AboutMySkills />
+                <AboutMyHobbies />
+            </div>
+            <SectionNav path="contact"/>
+        </div>
     </div>
 </template>
 
 <script setup>
-    const loaderText = useLoaderText();
-    loaderText.value = 'About';
+// import data from '~/assets/data.json';
+
+const isLoading = useIsLoading();
+const loaderText = useLoaderText();
+loaderText.value = 'About';
+
+const canScroll = ref(true);
+const container = ref(null);
+
+onMounted(()=> {
+    canScroll.value = container.value.scrollHeight > document.body.clientHeight;
+})
 </script>
 
 <style scoped>
-
 </style>
