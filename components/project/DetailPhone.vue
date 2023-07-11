784b170ead1976b1f942b647dad299709f6395ef
yukilun <102889935+yukilun@users.noreply.github.com>
2023-07-11 02:03:25 -0700

    change margin for phone details to percentage

--- components/project/DetailPhone.vue
+++ components/project/DetailPhone.vue
@@ -1,5 +1,5 @@
 <template>
-    <ScrollToggle class="project-detail flex-1 max-w-[300px] aspect-phone my-5 rounded-2xl shadow-lg overflow-hidden" :class="index % 2 != 0 ? `mt-24` : `mb-24` ">
+    <ScrollToggle class="project-detail flex-1 max-w-[300px] aspect-phone my-5 rounded-lg shadow-lg overflow-hidden" :class="index % 2 != 0 ? `mt-[5%]` : `mb-[5%]` ">
         <img :src="`${images.front}`" :alt="`${projectName} phone detail ${index+1} front`" class="front-image"/>
         <img :src="`${images.back}`" :alt="`${projectName} phone detail ${index+1} back`" class="back-image"/>
     </ScrollToggle>
