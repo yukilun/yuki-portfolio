85f180512fc4f14a4153556393933a48d6a594d3
yukilun <102889935+yukilun@users.noreply.github.com>
2023-07-11 00:45:50 -0700

    add content for each project

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -1,13 +1,33 @@
 <template>
-    <div class="content-container">
-        Project {{ id }}
+    <div class="content-container overflow-hidden" :class="isLoading && 'h-0'" ref="container">
+        <div class="project pt-[150px] md:pt-[200px] w-[95%] mx-auto">
+            <ProjectHeader :project="project"/>
+            <ProjectMockups v-if="project.mockups.tablet" :project="project"/>
+            <ProjectPrototypeMockups v-else :project="project"/>
+            <ProjectInfo :project="project"/>
+            <ProjectVideo v-if="project.video" :project="project"/>
+            <ProjectPrototype v-if="project.prototype" :project="project"/>
+            <ProjectDetails :project="project"/>
+            <ProjectNav :prevPath="prevPath" :nextPath="nextPath"/>
+        </div>
     </div>
 </template>
 
 <script setup>
-    const { id } = useRoute().params;    
-    const loaderText = useLoaderText();
-    loaderText.value = `Project ${id}`;
+import data from '~/assets/data.json';
+const { id } = useRoute().params;
+const projectIndex = data.projects.findIndex(project => project.id == id);
+if(projectIndex < 0) {
+    throw createError({statusCode: 404, statusMessage: 'Project not found', fatal: true});
+} 
+const project = data.projects[projectIndex];
+const prevPath = projectIndex <= 0 ? null : `projects/${data.projects[projectIndex - 1].id}`;
+const nextPath = projectIndex >= data.projects.length - 1 ? null : `projects/${data.projects[projectIndex + 1].id}`;
+
+const isLoading = useIsLoading();
+const loaderText = useLoaderText();
+loaderText.value = `${project.name}`;
+
 </script>
 
 <style scoped>
