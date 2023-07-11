750099f466444308ccd57d9c2a2f7200a30007d2
yukilun <102889935+yukilun@users.noreply.github.com>
2023-07-11 00:45:37 -0700

    adjust sizes for project thumbnails

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="content-container" :class="isLoading && 'h-0 overflow-hidden'" ref="container">
-        <div class="projects w-[90%] mx-auto max-w-[1200px]">
+        <div class="projects w-[95%] mx-auto">
             <SectionHeader heading="Projects" :canScroll="canScroll" />
-            <div class="projects-table">
-                <ProjectsRow v-for="project in data.projects" :project="project" />
+            <div class="projects-thumbnails max-w-[1400px] mx-auto">
+                <ProjectThumbnailRow v-for="project in data.projects" :project="project" />
             </div>
+            <SectionNav path="about"/>
         </div>
     </div>
 </template>
