1f2a3eff7301922196e29522a8548a716ac43fe3
yukilun <102889935+yukilun@users.noreply.github.com>
2023-07-12 02:09:23 -0700

    fix image url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,12 +18,12 @@ export default defineNuxtConfig({
         { name: "twitter:site", content: "@yukilun" },
         { name: "twitter:title", content: "Yuki Lun's Portfolio" },
         { name: "twitter:description", content: "Welcome to Yuki Lun's portfolio." },
-        { name: "twitter:image", content: process.env.URL + "images/logo.png" },
+        { name: "twitter:image", content: process.env.URL + "/images/logo.png" },
         { property: "og:type", content: "profile" },
         { property: "og:title", content: "Yuki Lun's Portfolio" },
         { property: "og:description", content: "Welcome to Yuki Lun's portfolio." },
         { property: "og:url", content: process.env.URL  },
-        { property: "og:image", content: process.env.URL + "images/logo.png" },
+        { property: "og:image", content: process.env.URL + "/images/logo.png" },
       ],
       link: [
         { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png"},
@@ -38,6 +38,6 @@ export default defineNuxtConfig({
   runtimeConfig: {
     EMAIL: process.env.EMAIL,
     EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
-    URL:process.env.URL,
+    URL:process.env.URL
   },
 })
