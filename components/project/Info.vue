2b5d73b0e24689e86c0e2bd6babc31fe0a8c4960
yukilun <102889935+yukilun@users.noreply.github.com>
2023-07-14 23:49:10 -0700

    Fix typo (link to links)

--- components/project/Info.vue
+++ components/project/Info.vue
@@ -6,7 +6,7 @@
         </div>       
         <div class="flex-1 basis-5/12 md:order-1">
             <div class="project-links border-t-2 border-[#b9a859] py-4 mb-4">
-                <h2 class="text-2xl sm:text-3xl md:text-4xl mb-4">Link</h2> 
+                <h2 class="text-2xl sm:text-3xl md:text-4xl mb-4">Links</h2> 
                 <div class="flex flex-wrap gap-5 sm:text-lg md:text-xl">
                     <a v-if="project.links.live" :href="`${project.links.live}`" class="project-link" target="_blank">Live Website</a>
                     <a v-if="project.links.repo" :href="`${project.links.repo}`" class="project-link" target="_blank">GitHub Repo</a>
