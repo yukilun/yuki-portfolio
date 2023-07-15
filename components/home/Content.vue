bcec8b185ca07d21a1d9e936ce3a8da5f51615d1
yukilun <102889935+yukilun@users.noreply.github.com>
2023-07-14 22:31:36 -0700

    update heading tag

--- components/home/Content.vue
+++ components/home/Content.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="home-content flex-shrink-0 flex-grow pb-20 lg:pb-0 lg:pl-10">
         <h1 class="home-content-intro-1 text-4xl md:text-5xl lg:text-6xl xl:text-7xl capitalize mb-3 md:mb-5">I'm <span class="text-highlight">Yuki</span>,</h1>
-        <h3 class="home-content-intro-2 text-3xl md:text-4xl lg:text-5xl xl:text-6xl capitalize mb-1 md:mb-3">a full stack</h3>
-        <h3 class="home-content-intro-3 text-3xl md:text-4xl lg:text-5xl xl:text-6xl capitalize mb-10 md:mb-14 lg:mb-20"><span class="text-highlight">web developer</span>.</h3>
+        <h2 class="home-content-intro-2 text-3xl md:text-4xl lg:text-5xl xl:text-6xl capitalize mb-1 md:mb-3">a full stack</h2>
+        <h2 class="home-content-intro-3 text-3xl md:text-4xl lg:text-5xl xl:text-6xl capitalize mb-10 md:mb-14 lg:mb-20"><span class="text-highlight">web developer</span>.</h2>
         <NuxtLink to="/projects" @mouseenter="cursorIsHovering = true" @mouseleave="cursorIsHovering = false" 
             class="home-content-projects-button button text-xl md:text-2xl lg:text-3xl font-serif py-3 px-6 group" id="home-content-projects-button">
             View My Projects&nbsp;
--- components/project/Header.vue
+++ components/project/Header.vue
@@ -1,8 +1,8 @@
 <template>
     <ScrollToggle class="project-header mb-14 lg:mb-20">
         <h1 class="text-4xl sm:text-5xl md:text-6xl mb-4 sm:mb-8 text-highlight uppercase !leading-none">{{ project.name }}</h1>
-        <h3 class="text-lg sm:text-2xl md:text-3xl mb-4 !leading-none">{{ project.description }}</h3>
-        <h3 class="text-lg sm:text-2xl md:text-3xl !leading-none">{{ project.role }}</h3>
+        <h2 class="text-lg sm:text-2xl md:text-3xl mb-4 !leading-none">{{ project.description }}</h2>
+        <h2 class="text-lg sm:text-2xl md:text-3xl !leading-none">{{ project.role }}</h2>
     </ScrollToggle>
 </template>
 
