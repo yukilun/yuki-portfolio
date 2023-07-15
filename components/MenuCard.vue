a109aa10e5ee379c63d1abfeccbbb7288543b2ad
yukilun <102889935+yukilun@users.noreply.github.com>
2023-07-14 23:53:57 -0700

    add transition to menu card

--- components/MenuCard.vue
+++ components/MenuCard.vue
@@ -2,7 +2,7 @@
     <NuxtLink 
         :to="path === 'home' ? '/': `/${path}`" 
         @click="isOpenMenu = !isOpenMenu" @mouseenter="cursorIsHovering = true" @mouseleave="cursorIsHovering = false"
-        class="menu-card flex-shrink-0 scale-95 opacity-80 hover:scale-100 hover:opacity-100">
+        class="menu-card flex-shrink-0 scale-95 opacity-80 hover:scale-100 hover:opacity-100 transition-all">
         <div>
             <p class="font-serif text-base md:text-lg tracking-widest uppercase mb-2 text-white">{{ path }}</p>
             <img class="w-[400px] aspect-video object-cover hidden md:block" :src="`/images/menu/${path}-labtop.png`"
