bd77a8fc65f4f733547ea374c274290d766bb545
yukilun <102889935+yukilun@users.noreply.github.com>
2023-07-18 20:21:46 -0700

    add 3D transform on Hover for Project Thumbnail

--- components/project/ThumbnailRow.vue
+++ components/project/ThumbnailRow.vue
@@ -2,8 +2,8 @@
     <NuxtLink :to="`/projects/${project.id}`" @mouseenter="cursorText='View More'" @mouseleave="cursorText=null" class="cursor-none">
         <ScrollParallax :translateYpc="20" :opacityFrom="0.7" :opacityTo="1"
             class="project-thumbnail-row h-fit flex flex-col justify-center relative mb-16 md:mb-32 group"
-            :class="project.id % 2 !== 0 ? `items-end` : `items-start`">
-            <div class="project-thumbnail shadow-lg bg-[#e2be8e] w-[85%] md:w-[70%] aspect-[6/4] overflow-hidden relative flex flex-col justify-center items-center duration-700 scale-100 group-hover:scale-105 transition-all">
+            :class="project.id % 2 !== 0 ? `items-end` : `items-start`" :id="`project-thumbnail-${project.id}`">
+            <div class="project-thumbnail shadow-lg bg-[#cabb78] w-[85%] md:w-[70%] aspect-[6/4] overflow-hidden relative flex flex-col justify-center items-center"> <!--duration-700 scale-100 group-hover:scale-105 transition-all-->
                 <img class="target w-full h-[150%] object-cover" :src="`${project.thumbnail}`" alt="">
                 <div class="white-filter w-full h-full absolute top-0 left-0 bg-neutral-100 opacity-10 transition-all transition-500 group-hover:opacity-0"></div>
             </div>
@@ -35,9 +35,35 @@
 <script setup>
 const { project } = defineProps(['project']);
 const cursorText = useCursorText();
+onMounted(()=> {
+    const THRESHOLD = 10;
+    const thumbnail = document.querySelector(`#project-thumbnail-${project.id}`);
+    const handleHover = (e) => {
+        const { clientX, clientY, currentTarget } = e;
+        const { clientWidth, clientHeight, scrollLeft, scrollTop } = currentTarget;
+        const horizontal = (clientX - scrollLeft) / clientWidth;
+        const vertical = (clientY - scrollTop) / clientHeight;
+        const rotateX = (THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(2);
+        const rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(2);
+        thumbnail.style.transform = `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`;
+    }
+    const resetStyles = (e) => {
+        thumbnail.style.transform = `perspective(${e.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`;
+    }
+    thumbnail.addEventListener("mousemove", handleHover);
+    thumbnail.addEventListener("mouseleave", resetStyles);
+});
 </script>
 
 <style scoped>
+
+@media(hover:hover) {
+    .project-thumbnail-row {
+        transition: transform 0.1s ease;
+    }
+
+}
+
 .project-type-left,
 .project-name-left,
 .project-type-right,
